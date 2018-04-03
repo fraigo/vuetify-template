8a2955125d4857c256ba1c3785a1e9c0f42089d9
Francisco Igor <franciscoigor@gmail.com>
2018-04-02 17:28:28 -0700

    index.js - Changed destination folder to www

--- config/index.js
+++ config/index.js
@@ -1,15 +1,16 @@
 'use strict'
 
 // see http://vuejs-templates.github.io/webpack for documentation.
-const path = require('path')
+const path = require('path');
+
 
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../www/index.html'),
+    assetsRoot: path.resolve(__dirname, '../www'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
