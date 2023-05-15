7f6b2a9bdba885d110279e1feedd372e50a3d62d
Francisco Igor <franciscoi@tbadigital.com>
2023-05-15 16:15:20 -0700

    gh-pages

--- gh-pages.sh
+++ gh-pages.sh
@@ -1,3 +1,4 @@
+# publish GitHub pages
 git branch -D gh-pages > /dev/null
 git push origin --delete gh-pages
 git checkout -b gh-pages && npm run build && git add -f dist && git commit -m 'gh-pages' && git push origin gh-pages 
